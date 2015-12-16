e572557c696cba4cc52394b99e0f1d1e8ab66967
thomasgalliker <thomas@bluewin.ch>
2015-12-16 23:19:19 +0100

    Add static diacritics accessor

---
+++ Diacritics/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Diacritics.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool HasDiacritics(this string source)
+        {
+            return StaticDiacritics.Current.HasDiacritics(source);
+        }
+
+        public static string RemoveDiacritics(this string source)
+        {
+            return StaticDiacritics.Current.RemoveDiacritics(source);
+        }
+    }
+}
