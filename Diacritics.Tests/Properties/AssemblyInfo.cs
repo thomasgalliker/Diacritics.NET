7298108fdef0ee9c6a477e06e17fec9e9f205ee5
thomasgalliker <thomas@bluewin.ch>
2016-01-07 23:24:41 +0100

    Update project details (nuspec + AssemblyInfo)

--- Diacritics.Tests/Properties/AssemblyInfo.cs
+++ Diacritics.Tests/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Thomas Galliker")]
 [assembly: AssemblyProduct("Diacritics.Tests")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
