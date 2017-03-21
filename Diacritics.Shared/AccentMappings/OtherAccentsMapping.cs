f71d794492d74f07e0ce8f82b7f783de0a32e737
Aleksandr Moshkin <moshkin_alexander@outlook.com>
2017-03-21 17:20:40 +0300

    Other accents mapping, that was missed added

---
+++ Diacritics.Shared/AccentMappings/OtherAccentsMapping.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Diacritics.AccentMappings
+{
+    public class OtherAccentsMapping : IAccentMapping
+    {
+        private static readonly IDictionary<char, char> MappingDictionary = new Dictionary<char, char>
+        {
+            { 'ė', 'e' },
+            { 'ÿ', 'y' },
+            { 'ū', 'u' },
+            { 'ī', 'i' },
+            { 'į', 'i' },
+            { 'ø', 'i' },
+            { 'ō', 'o' },
+            { 'å', 'a' },
+            { 'ā', 'a' }
+        };
+
+        public IDictionary<char, char> Mapping { get { return MappingDictionary; } }
+    }
+}
