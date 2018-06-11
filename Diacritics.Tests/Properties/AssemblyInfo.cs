5395e1bf22cf6d8a20cf6499ae54787d18612671
Thomas Galliker <thomas@bluewin.ch>
2018-06-11 07:04:21 +0200

    Update AssemblyInfo

--- Diacritics.Tests/Properties/AssemblyInfo.cs
+++ Diacritics.Tests/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Thomas Galliker")]
 [assembly: AssemblyProduct("Diacritics.Tests")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
