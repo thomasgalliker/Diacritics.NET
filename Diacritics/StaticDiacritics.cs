c9065ed8d2e29610944782472be83a91d2f9a90d
Thomas Galliker <thomas@bluewin.ch>
2020-11-28 15:49:37 +0100

    Mark staticdiacritics as obsolete

--- Diacritics/StaticDiacritics.cs
+++ Diacritics/StaticDiacritics.cs
@@ -3,8 +3,28 @@ using System.Threading;
 
 namespace Diacritics
 {
+    [Obsolete("Use DiacriticsMapper instead")]
     public static class StaticDiacritics
     {
+        private static Lazy<IDiacriticsMapper> implementation;
 
+        static StaticDiacritics()
+        {
+            SetDefaultMapper(CreateDefaultDiacriticsMapper);
+        }
+
+        [Obsolete("Use DiacriticsMapper.Current instead")]
+        public static IDiacriticsMapper Current => implementation.Value;
+        
+        [Obsolete("Use DiacriticsMapper.SetDefaultMapper instead")]
+        public static void SetDefaultMapper(Func<IDiacriticsMapper> factory)
+        {
+            implementation = new Lazy<IDiacriticsMapper>(factory, LazyThreadSafetyMode.PublicationOnly);
+        }
+
+        private static IDiacriticsMapper CreateDefaultDiacriticsMapper()
+        {
+            return new DefaultDiacriticsMapper();
+        }
     }
 }
