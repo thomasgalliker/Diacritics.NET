7294d02f4ad74a17004ca8e7e0cc7838e6ac5905
Thomas Galliker <>
2025-06-20 09:49:44 +0200

    Add IDiacriticsMapper.Current

--- Diacritics/IDiacriticsMapper.cs
+++ Diacritics/IDiacriticsMapper.cs
@@ -4,6 +4,13 @@ namespace Diacritics
 {
     public interface IDiacriticsMapper : IEnumerable<KeyValuePair<char, MappingReplacement>>
     {
+#if NET7_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Gets the singleton instance of <see cref="IDiacriticsMapper"/>.
+        /// </summary>
+        public static IDiacriticsMapper Current { get; } = DiacriticsMapper.Current;
+#endif
+
         string RemoveDiacritics(string source, DiacriticsOptions options = null);
 
         bool HasDiacritics(string source, DiacriticsOptions options = null);
