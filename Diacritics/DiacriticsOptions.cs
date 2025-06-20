2438b3652a0c32e33342a2c36b13ac8985c65ac4
Thomas Galliker <>
2025-06-20 13:00:52 +0200

    Add comment

--- Diacritics/DiacriticsOptions.cs
+++ Diacritics/DiacriticsOptions.cs
@@ -2,6 +2,10 @@
 {
     public class DiacriticsOptions
     {
+        /// <summary>
+        /// Indicates if the <see cref="MappingReplacement.Base"/> value or <see cref="MappingReplacement.Decompose"/> should be used.
+        /// This option is only relevant for accent mappings which have <see cref="MappingReplacement.Decompose"/> values defined (e.g. <see cref="Diacritics.AccentMappings.GermanAccentsMapping"/>).
+        /// </summary>
         public bool Decompose { get; set; }
     }
 }
