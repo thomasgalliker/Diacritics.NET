f91fc08ce0f70a9c02241062927d897bd2a27d34
Thomas Galliker <>
2025-06-20 13:13:08 +0200

    Add equality comparison in MappingReplacement

--- Diacritics/Model/MappingReplacement.cs
+++ Diacritics/Model/MappingReplacement.cs
@@ -1,6 +1,8 @@
-﻿namespace Diacritics
+﻿using System;
+
+namespace Diacritics
 {
-    public struct MappingReplacement
+    public struct MappingReplacement : IEquatable<MappingReplacement>
     {
         public MappingReplacement(string @base, string decompose, string decomposeTitle) : this()
         {
@@ -16,5 +18,38 @@
         public string DecomposeTitle { get; set; }
 
         public static implicit operator MappingReplacement(string value) => new MappingReplacement(value, null, null);
+
+        public bool Equals(MappingReplacement other)
+        {
+            return string.Equals(this.Base, other.Base, StringComparison.Ordinal) &&
+                   string.Equals(this.Decompose, other.Decompose, StringComparison.Ordinal) &&
+                   string.Equals(this.DecomposeTitle, other.DecomposeTitle, StringComparison.Ordinal);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is MappingReplacement other && this.Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (this.Base != null ? StringComparer.Ordinal.GetHashCode(this.Base) : 0);
+                hashCode = (hashCode * 397) ^ (this.Decompose != null ? StringComparer.Ordinal.GetHashCode(this.Decompose) : 0);
+                hashCode = (hashCode * 397) ^ (this.DecomposeTitle != null ? StringComparer.Ordinal.GetHashCode(this.DecomposeTitle) : 0);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(MappingReplacement left, MappingReplacement right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(MappingReplacement left, MappingReplacement right)
+        {
+            return !left.Equals(right);
+        }
     }
 }
