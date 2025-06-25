555ab1a6967bb391ac85c403682b16b063dd0402
Thomas Galliker <>
2025-06-25 10:41:16 +0200

    Add benchmark project to track performance improvements

---
+++ Tests/Diacritics.Benchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using Diacritics.Benchmark;
+
+namespace Diacritical.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<RemoveDiacriticsBenchmark>();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
